94423696a15864f9f8a54d5c18ea60b84a1739a7
Chaimaa Nairi <83820363+chaimaanairi@users.noreply.github.com>
2025-03-31 06:51:09 +0200

    2_nftMarket_migration.js

--- migrations/2_nftMarket_migration.js
+++ migrations/2_nftMarket_migration.js
@@ -1,5 +1,8 @@
+// Import the NftMarket contract. This is the smart contract for the NFT marketplace.
 const NftMarket = artifacts.require("NftMarket");
 
+// Export a function to handle the deployment of the NftMarket contract.
 module.exports = function (deployer) {
+  // Deploy the NftMarket contract to the blockchain.
   deployer.deploy(NftMarket);
 };
