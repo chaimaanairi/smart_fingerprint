08548266f63a48eab195d3d4ef3755e2ff06cbcf
Chaimaa Nairi <83820363+chaimaanairi@users.noreply.github.com>
2025-03-31 06:50:44 +0200

    1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,8 @@
+// Import the Migrations contract. This is a default contract provided by Truffle to handle migrations.
 const Migrations = artifacts.require("Migrations");
 
+// Export a function that will deploy the Migrations contract to the blockchain.
 module.exports = function (deployer) {
+  // Deploy the Migrations contract. The deployer object handles the deployment process.
   deployer.deploy(Migrations);
 };
