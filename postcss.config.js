2bba044b882bea2e4eeeb83db46c672581866d22
Chaimaa Nairi <83820363+chaimaanairi@users.noreply.github.com>
2025-03-31 07:11:54 +0200

    postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
+  // Plugins to use for PostCSS
   plugins: {
+    // Tailwind CSS plugin - Generates the necessary styles based on the Tailwind configuration
     tailwindcss: {},
+
+    // Autoprefixer plugin - Adds vendor prefixes to CSS for better browser compatibility
     autoprefixer: {},
   },
 }
