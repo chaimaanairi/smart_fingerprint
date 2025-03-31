615e837019e451b896ca862c365698d557bcb28d
Chaimaa Nairi <83820363+chaimaanairi@users.noreply.github.com>
2025-03-31 07:10:06 +0200

    truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,18 +1,20 @@
-
 module.exports = {
-  contracts_build_directory: "./public/contracts",
+  // Directory to store the compiled contract artifacts
+  contracts_build_directory: "./public/contracts",  // Define the path where compiled contract files will be stored
 
   networks: {
+    // Configuration for the development network
     development: {
-    host: "127.0.0.1",     
-    port: 7545,            
-    network_id: "*",     
+      host: "127.0.0.1",    // Localhost address, pointing to the local machine
+      port: 7545,           // Port number where Ganache (or your local Ethereum network) is running
+      network_id: "*",      // Accept any network ID, useful for local development or testing
     },
   },
 
   compilers: {
     solc: {
-      version: "0.8.17" 
+      // Specify the Solidity compiler version to use
+      version: "0.8.17"  // Version of Solidity compiler
     }
   }
 
