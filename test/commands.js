860bc1221493ea83b6bf7c3230db43df096c43e8
Chaimaa Nairi <83820363+chaimaanairi@users.noreply.github.com>
2025-03-31 06:54:33 +0200

    commands.js

--- test/commands.js
+++ test/commands.js
@@ -7,7 +7,8 @@ const instance = await NftMarket.deployed();
 
 // listing price is 2.5 ether = 25000000000000000 wei
 
-
+// Minting the first NFT with token URI and price set to 0.5 ether (500000000000000000 wei)
+// Also, paying the listing price of 2.5 ether (25000000000000000 wei)
 instance.mintToken("https://gateway.pinata.cloud/ipfs/QmNzgSu9T3euR1SKhNfaZgDzTnFXZm7JoWYnrisAFDWpT3","500000000000000000", {value: "25000000000000000",from: accounts[0]})
 
 instance.mintToken("https://gateway.pinata.cloud/ipfs/QmdHm7oWwaAFm9ErPXEx8DDYe6wNH9CCQckQwe29LKPZbz","600000000000000000", {value: "25000000000000000",from: accounts[0]})
